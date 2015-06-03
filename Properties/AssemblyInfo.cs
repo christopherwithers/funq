48eb4af29a62408257e851a323cf34c0eb4fa141
Chris Withers <chris.withers@gmail.com>
2015-06-03 11:23:24 +0100

    Forked from http://funq.codeplex.com/

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,5 @@
+﻿
+using System.Reflection;
+
+[assembly: AssemblyTitle("Funq")]
+[assembly: AssemblyDescription("The fastest .NET and Silverlight dependency injection library available, based on lambda factories.")]
